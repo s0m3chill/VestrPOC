d59edbb28c9e13e7a3fca4a4a17af4ccc60e253f
maksymkordiyak <maksym.kordiyak@vestr.com>
2021-11-11 16:38:17 +0200

    add bottom nav, a chart

---
+++ lib/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const vestrBlue = Color(0xFF0B79D0);
+const vestrSurfaceWhite = Color(0xFFFFFBFA);
+const vestrGrey = Color(0x8A000000);
+
+const kShrinePink100 = Color(0xFFFEDBD0);
+const kShrinePink300 = Color(0xFFFBB8AC);
+const kShrinePink400 = Color(0xFFEAA4A4);
+
+const kShrineBrown900 = Color(0xFF442B2D);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+
+const kShrineBackgroundWhite = Colors.white;
